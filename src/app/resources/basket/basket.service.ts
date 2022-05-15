5896230b5b6470f237a1f7e7da3fa21a9d45774f
it-tim <timothe.molavy@gmail.com>
2022-05-15 19:54:51 +0200

    fix add same product same format in basket

--- src/app/resources/basket/basket.service.ts
+++ src/app/resources/basket/basket.service.ts
@@ -17,20 +17,19 @@ export class BasketService {
   addProduct(product: Illustration, selectedFormat: any, selectedQuantity: number) {
     const alreadyStoredItems = this.products$.getValue()
     const format = product.formats.filter(format => format.format === selectedFormat)[0]
-    let item = alreadyStoredItems.find(i => i._id === product._id && i.format.format._id === format.format)
-
+    let item = alreadyStoredItems.find(i => i._id === product._id && i.format.format === format.format)
     item = !item
-    ? {
-        _id: product._id,
-        label: product.label,
-        format: format,
-        image: product.images.packShot,
-        quantity: selectedQuantity,
+      ? {
+          _id: product._id,
+          label: product.label,
+          format: format,
+          image: product.images.packShot,
+          quantity: selectedQuantity,
       }
-    : {
-      ...item,
-      quantity: item.quantity + selectedQuantity,
-    }
+      : {
+        ...item,
+        quantity: item.quantity + selectedQuantity,
+      }
      
     const items = [
       ...alreadyStoredItems.filter(i => (i._id === item._id && i.format.format !== format.format) || i._id !== item._id),
