f41c9e336cdb11cb44aefd1ff8cfdaf06ed57d06
it-tim <timothe.molavy@gmail.com>
2022-05-15 20:46:16 +0200

    link image footer with random collection image

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -1,5 +1,9 @@
-import {Component} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {tap} from 'rxjs/operators'
+import {Observable, Subscription} from 'rxjs'
 
+import {Collection} from '@resources/collection/collection.model'
+import {CollectionService} from '@resources/collection/collection.service'
 import {environment} from '@env/environment'
 
 @Component({
@@ -7,9 +11,37 @@ import {environment} from '@env/environment'
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
   env = environment
+  backgroundImage = ''
+  
+  collectionsSubscription$: Subscription
 
-  constructor() { }
+  collections$: Observable<Collection[]> = this.collectionService.collections$.pipe(
+    tap(illustrations => {
+      if (!illustrations) {
+        this.collectionService.getCollections()
+      }
+    })
+  )
+
+  constructor(
+    private collectionService: CollectionService,
+  ) { }
+
+  ngOnInit(): void {
+    this.collectionsSubscription$ = this.collections$.subscribe(collections => {
+      if (collections?.length) {
+        const collectionImages = collections[Math.floor(Math.random() * collections.length)].images.details
+        if (collectionImages?.length) {
+          this.backgroundImage = this.env.uploads + encodeURI(collectionImages[Math.floor(Math.random() * collectionImages.length)])
+        }
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.collectionsSubscription$.unsubscribe()
+  }
 
 }
